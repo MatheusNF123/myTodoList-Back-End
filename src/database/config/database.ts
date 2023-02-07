f663996a816b633aef16e3d2de179e5050082ab2
Matheus <matheus_cs.nf@hotmail.com>
2023-02-07 11:11:00 -0300

    corrigindo conexao

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -4,12 +4,13 @@ import { Options } from 'sequelize';
 const { HOST, PASSWORD_POSTGRES, DATABASE, DB_USERNAME, DB_PORT } = process.env;
 
 const config: Options = {
-  username: DB_USERNAME || 'postgres',
-  password: PASSWORD_POSTGRES || 'meuDBTodoList101',
-  database: DATABASE || 'postgres',
-  host: HOST || 'db.svwbnipowtwtefwwybbf.supabase.co',
-  port: Number(DB_PORT) || 5432,
+  username: DB_USERNAME ,
+  password: PASSWORD_POSTGRES ,
+  database: DATABASE,
+  host: HOST ,
+  port: Number(DB_PORT) ,
   dialect: 'postgres',
 }
+
 
 module.exports = config;
