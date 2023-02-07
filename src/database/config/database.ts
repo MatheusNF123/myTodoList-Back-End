e7e0fc997d6abc18e34c29fbd79609d02c484a3e
Matheus <matheus_cs.nf@hotmail.com>
2023-02-07 13:43:32 -0300

    arrumando conexão com banco de dados

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -3,23 +3,13 @@ import { Options } from 'sequelize';
 
 const { HOST, PASSWORD_POSTGRES, DATABASE, DB_USERNAME, DB_PORT } = process.env;
 
-// const config: Options = {
-//   username: DB_USERNAME ,
-//   password: PASSWORD_POSTGRES ,
-//   database: DATABASE,
-//   host: HOST ,
-//   port: Number(DB_PORT) ,
-//   dialect: 'postgres',
-// }
-
 const config: Options = {
-  username: DB_USERNAME || process.env.DB_USER || 'root',
-  password: PASSWORD_POSTGRES || process.env.DB_PASS || 'password',
-  database: DATABASE || process.env.DB_NAME || 'todoList',
-  host: HOST || process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  dialect: 'mysql',
+  username: DB_USERNAME ,
+  password: PASSWORD_POSTGRES ,
+  database: DATABASE,
+  host: HOST ,
+  port: Number(DB_PORT) ,
+  dialect: 'postgres',
 }
-
 
 module.exports = config;
